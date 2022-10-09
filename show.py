4ea65c1d14689a517f67bd27add647066b20f6dd
Nelson VICEL--FARAH <nelson.vicel-farah@epita.fr>
2022-10-09 18:31:26 +0200

    on a bien codé

---
+++ show.py
@@ -0,0 +1,36 @@
+import json
+
+
+
+if __name__ == '__main__':
+
+    # display the image with the bounding boxes
+    def show(image, boxes):
+        import matplotlib.pyplot as plt
+        import matplotlib.patches as patches
+        from PIL import Image
+
+        # load the image
+        image = Image.open(image)
+
+        # get the context for drawing boxes
+        fig, ax = plt.subplots(1)
+        ax.imshow(image)
+
+        # plot each box
+        for box in boxes:
+            # get coordinates
+            x, y, width, height = box
+            # create the shape
+            rect = patches.Rectangle((x, y), width, height, linewidth=1, edgecolor='r', facecolor='none')
+            # draw the box
+            ax.add_patch(rect)
+
+        # show the plot
+        plt.show()
+
+    with open('data.json', 'r') as f:
+        data = json.load(f)
+
+    for image, boxes in data.items():
+        show(image, boxes)
